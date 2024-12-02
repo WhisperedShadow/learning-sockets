10c31cc5903cbe377347bbb34b96681db4117e75
WhisperedShadow <dd9514371@gmail.com>
2024-12-02 22:20:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+
+app.use(express.static('public'));
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chatMessage', ({ username, message }) => {
+        io.emit('chatMessage', { username, message });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
