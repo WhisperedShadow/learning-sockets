52eff5997165814247151b5d40833d7a3ae3d341
WhisperedShadow <dd9514371@gmail.com>
2024-12-03 20:36:15 +0530

    The files are modified

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
-
 const io = socketIo(server);
 
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('chatMessage', ({ username, message }) => {
-        io.emit('chatMessage', { username, message });
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("chatMessage", ({ username, message }) => {
+    io.emit("chatMessage", { username, message });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
